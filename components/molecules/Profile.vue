461ed94a85d36155e89cfcd87603b1295874634e
cassiocappellari <cassiocappellari@gmail.com>
2021-10-09 07:20:44 -0300

    feat: create profile component

---
+++ components/molecules/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="flex items-center">
+		<img src="https://github.com/cassiocappellari.png" alt="Cássio Cappellari" class="mr-6 w-20 h-20 rounded-full">
+		<div class="flex flex-col w-full">
+			<strong class="text-title text-2xl">Cássio Cappellari</strong>
+			<p class="flex items-center">
+				<img src="icons/level.svg" alt="level" class="mr-2">
+				<span class="text-base">Level {{ level }}</span>
+			</p>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapState } from 'vuex';
+
+export default Vue.extend({
+	computed: mapState('Challenges', ['level']),
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,31 @@
 <template>
-	<Tutorial />
+	<section class="flex flex-col lg:flex-row flex-1 lg:flex-none lg:mt-16 sm:gap-x-10 md:gap-x-20">
+		<div class="flex flex-col w-full lg:w-1/2">
+			<Profile />
+			<CompletedChallenges />
+		</div>
+	</section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
-export default Vue.extend({});
+import CompletedChallenges from '~/components/atoms/CompletedChallenges.vue';
+import Profile from '~/components/molecules/Profile.vue';
+
+interface Head {
+	title: string;
+}
+
+export default Vue.extend({
+	head (): Head {
+		return {
+			title: 'Home | movue.it',
+		};
+	},
+	components: {
+		CompletedChallenges,
+		Profile,
+	},
+});
 </script>
