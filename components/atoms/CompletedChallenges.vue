68c8b116e0d745c53febf39225db62bf4599600b
cassiocappellari <cassiocappellari@gmail.com>
2021-10-09 07:34:48 -0300

    feat: create completed challenges component

---
+++ components/atoms/CompletedChallenges.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="flex justify-between items-center pb-5 border-b-2 border-gray-line mt-8 lg:mt-14 font-medium">
+		<p class="text-lg">
+			Completed challenges
+		</p>
+		<p class="text-2xl">
+			{{ completedChallenges }}
+		</p>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapState } from 'vuex';
+
+export default Vue.extend({
+	computed: mapState('Challenges', ['completedChallenges']),
+});
+</script>
